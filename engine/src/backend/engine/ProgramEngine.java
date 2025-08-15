1a5d59f37f385392929f74f73c3b8871a2ed3227
Ofri Kuperberg <kuperbergof@mta.ac.il>
2025-08-15 23:54:23 +0300

    ProgramEngine start - still a lot left to do but good start

--- engine/src/backend/engine/ProgramEngine.java
+++ engine/src/backend/engine/ProgramEngine.java
@@ -1,20 +1,36 @@
 package backend.engine;
 
+import backend.system.generated.SInstruction;
 import backend.system.generated.SProgram;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class ProgramEngine
 {
     private String programName;
     private Map<String, Integer> contextMap = new HashMap<>();
-    private LinkedList<Command> commands = new LinkedList<>();
+    private List<Instruction> instructions = new LinkedList<>();
     private Set<String> labels = new HashSet<>();
     private statistics programStats = null;
 
     public ProgramEngine(SProgram program)
     {
         this.programName = program.getName();
+        instructions = program.getSInstructions().getSInstruction()
+                .stream()
+                .map(Instruction::createInstruction)
+                .collect(Collectors.toList());
+        labels = program.getSInstructions().getSInstruction()
+                .stream()
+                .map(SInstruction::getSLabel)
+                .filter(Objects::nonNull)
+                .collect(Collectors.toSet());
+        initializeContextMap();
 
+    }
+
+    private void initializeContextMap()
+    {
     }
 }
