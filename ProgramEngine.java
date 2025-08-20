87f37f5ee0f62acb50e92b0ad71b7c50ce04763f
daniel hershko <134381418+Danielhershko1@users.noreply.github.com>
2025-08-20 12:50:06 +0300

    Add files via upload

---
+++ ProgramEngine.java
@@ -0,0 +1,102 @@
+package backend.engine;
+
+import backend.system.generated.SInstruction;
+import backend.system.generated.SProgram;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class ProgramEngine
+{
+    private int totalCycels = 0;
+    private final String programName;
+    private Map<String, Integer> contextMap = new HashMap<>();
+    private List<Instruction> instructions;
+    private Set<String> labels;
+    private statistics programStats = new statistics();
+    private final String outputName = "y";
+    private final String EXITLabelName = "EXIT";
+
+    public ProgramEngine(SProgram program)
+    {
+        this.programName = program.getName();
+        instructions = program.getSInstructions().getSInstruction()
+                .stream()
+                .map(Instruction::createInstruction)
+                .collect(Collectors.toList());
+        labels = program.getSInstructions().getSInstruction()
+                .stream()
+                .map(SInstruction::getSLabel)
+                .filter(Objects::nonNull)
+                .filter(label -> label.startsWith("L"))
+                .collect(Collectors.toSet());
+        initializeContextMap();
+
+    }
+
+    private void initializeContextMap()
+    {
+        contextMap.clear();
+        contextMap.put(outputName, 0);
+        contextMap.put(Instruction.ProgramCounterName, 0); // Program Counter
+        for (int instruction_index = 0; instruction_index < instructions.size(); instruction_index++)
+        {
+            Instruction instruction = instructions.get(instruction_index);
+            contextMap.put(instruction.getMainVarName(), 0);
+            if (!instruction.getLabel().isEmpty())
+            {
+                contextMap.put(instruction.getLabel(), instruction_index);
+            }
+            for (String argName : instruction.getArgs().values())
+            {
+                if (!contextMap.containsKey(argName) && !isLabelArgument(argName))
+                {
+                    contextMap.put(argName, 0); // initialize all work/unhandled input variables to 0
+                }
+                if (argName.equals(EXITLabelName))
+                {
+                    contextMap.put(EXITLabelName, instructions.size()); // EXIT label is set to the end of the program
+                }
+            }
+        }
+    }
+
+    private boolean isLabelArgument(String argName)
+    {
+        return labels.contains(argName) || argName.startsWith("L");
+    }
+
+    public void run()
+    {
+        programStats.incrementExecutionCount();
+        while(contextMap.get(Instruction.ProgramCounterName) < instructions.size())
+        {
+            int currentPC = contextMap.get(Instruction.ProgramCounterName);
+            Instruction instruction = instructions.get(currentPC);
+            try
+            {
+                instruction.execute(contextMap);
+                totalCycels = totalCycels + instruction.getCycles();
+                programStats.incrementCycles(instruction.getCycles());
+            } catch (IllegalArgumentException e)
+            {
+                throw new RuntimeException("Error executing instruction at PC=" + currentPC + ": " + e.getMessage(), e);
+            }
+        }
+        programStats.setY(contextMap.get(outputName));
+    }
+
+    // TODO - delete this method before commiting
+    public void printProgram()
+    {
+        System.out.println("Program Name: " + programName);
+        System.out.println("Instructions:");
+        for (int i = 0; i < instructions.size(); i++)
+        {
+           System.out.println(instructions.get(i).getDisplayFormat(i+1));
+        }
+        System.out.println("Context Map: " + contextMap);
+        System.out.println("Labels: " + labels);
+        System.out.println("Total Cycels: " + totalCycels);
+    }
+}
