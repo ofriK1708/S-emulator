7645716d6c756352d4636bfcb318dadd66dbd3d6
Ofri Kuperberg <kuperbergof@mta.ac.il>
2025-09-05 19:10:24 +0300

    merged Ofri and Daniel work

---
+++ ui/src/jfx/program/function/ProgramFunctionController.java
@@ -0,0 +1,35 @@
+package jfx.program.function;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class ProgramFunctionController {
+
+    @FXML
+    private Button collapseButton;
+
+    @FXML
+    private Button collapseButton1;
+
+    @FXML
+    private Label degreeInfoLabel;
+
+    @FXML
+    private Label degreeInfoLabel1;
+
+    @FXML
+    private Button expandButton;
+
+    @FXML
+    void handleCollapse(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleExpand(ActionEvent event) {
+
+    }
+
+}
